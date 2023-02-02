e0eeb3b5fb8053ec5360626d7618fc4a0654d1a9
Lee William <lwilliam@u90z03s05.42kl.edu.my>
2023-02-02 16:45:36 +0800

    WrongAnimal WIP

---
+++ CPP_04/ex01/Animals.cpp
@@ -0,0 +1,67 @@
+#include "Animals.hpp"
+
+Animal::Animal()
+{
+	std::cout << "Default constructor is called" << std::endl;
+}
+
+Animal::Animal(const Animal &A)
+{
+	std::cout << "Copy constructor is called" << std::endl;
+	*this = A;
+}
+
+Animal &Animal::operator = (const Animal &A)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &A)
+		type = A.type;
+	return (*this);
+}
+
+void Animal::makeSound() const
+{
+	;
+}
+
+std::string Animal::getType() const
+{
+	return (type);
+}
+
+Animal::~Animal()
+{
+	std::cout << "Destructor is called" << std::endl;
+}
+
+Dog::Dog()
+{
+	std::cout << "Dog constructor is called" << std::endl;
+	Animal::type = "Dog";
+}
+
+void Dog::makeSound() const
+{
+	std::cout << "Woof!!!!" << std::endl;
+}
+
+Dog::~Dog()
+{
+	std::cout << "Dog detructor is called" << std::endl;
+}
+
+Cat::Cat()
+{
+	std::cout << "Cat constructor is called" << std::endl;
+	Animal::type = "Cat";
+}
+
+void Cat::makeSound() const
+{
+	std::cout << "Meow...." << std::endl;
+}
+
+Cat::~Cat()
+{
+	std::cout << "Cat detructor is called" << std::endl;
+}
---
+++ CPP_04/ex01/Animals.hpp
@@ -0,0 +1,36 @@
+#ifndef ANIMALS_HPP
+#define ANIMALS_HPP
+
+#include <iostream>
+
+class Animal
+{
+	public:
+		Animal();
+		Animal(const Animal &A);
+		Animal &operator = (const Animal &A);
+		virtual void makeSound() const;
+		std::string getType() const;
+		~Animal();
+	protected:
+		std::string type;
+};
+
+class Dog : public Animal
+{
+	public:
+		Dog();
+		void makeSound() const;
+		~Dog();
+};
+
+class Cat : public Animal
+{
+	public:
+		Cat();
+		void makeSound() const;
+		~Cat();
+};
+
+
+#endif
